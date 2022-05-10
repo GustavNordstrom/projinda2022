3b9a9c87896142e1aee6b411bcdec7b7622541d3
Nicholas Weis Shore <nweisshore@hotmail.com>
2022-05-10 10:44:59 +0200

    Fix platform collision
    Player only jumps when above platform and falling

--- core/src/com/badlogic/doodlejump/World.java
+++ core/src/com/badlogic/doodlejump/World.java
@@ -3,6 +3,8 @@ package com.badlogic.doodlejump;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
+import java.util.Random;
+
 public class World {
     public static final Vector2 gravity = new Vector2(0, -300);
 
@@ -10,13 +12,19 @@ public class World {
     public Array<Platform> platforms;
 
     public World(){
-        this.player = new Player(300, 400);
+        this.player = new Player(250, 200);
         this.platforms = new Array<>();
         generateWorld();
     }
 
     private void generateWorld(){
-        Platform platform = new Platform(300, 100);
-        platforms.add(platform);
+        //Generate starting platform
+        Platform firstPlat = new Platform(250, 0);
+        platforms.add(firstPlat);
+        Random rng = new Random();
+        for (int i = 1; i < 16; i++) {
+            Platform platform = new Platform(rng.nextInt(400), rng.nextInt(100) + 200*i);
+            platforms.add(platform);
+        }
     }
 }
